0257ea975a87764cfa9a27066c8f8315cdbc07ca
tamjidahmed <mail.to.tamjid@gmail.com>
2022-03-18 11:01:18 +0600

    fix build

--- grpc_cosmos_sdk_v44.go
+++ grpc_cosmos_sdk_v44.go
@@ -33,8 +33,8 @@ import (
 	gaia "github.com/cosmos/gaia/v6/app"
 	sdkutilities "github.com/emerishq/sdk-service-meta/gen/sdk_utilities"
 	liquidity "github.com/gravity-devs/liquidity/x/liquidity/types"
-	osmomint "github.com/osmosis-labs/osmosis/v7/x/mint/types"
 	irismint "github.com/irisnet/irishub/modules/mint/types"
+	osmomint "github.com/osmosis-labs/osmosis/v7/x/mint/types"
 	"github.com/tendermint/tendermint/abci/types"
 	"google.golang.org/grpc"
 )
